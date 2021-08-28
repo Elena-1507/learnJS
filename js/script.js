429b10fc84e83ae513d40a7ef08e26e35e8ff61d
Lena <eln99@bk.ru>
2021-08-28 14:48:16 +0300

    add for, if, else

--- js/script.js
+++ js/script.js
@@ -10,11 +10,29 @@ let PersonalMovieDB = {
     privat: false,
 };
 
-let movie1 = prompt('Один из последних просмотренных фильмов?', ""),
-    grade1 = prompt('На сколько его оцените?', ""),
-    movie2 = prompt('Один из последних просмотренных фильмов?', ""),
-    grade2 = prompt('На сколько его оцените?', "");
 
-PersonalMovieDB.movies[movie1] = grade1;
-PersonalMovieDB.movies[movie2] = grade2;
-    console.log(PersonalMovieDB);
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', ""),
+        b = prompt('На сколько его оцените?', "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        PersonalMovieDB.movies[a] = b;
+        console.log('yes!!!!');
+    } else{
+        console.log('nooooo');
+        i--;
+    }
+        
+}
+
+if (PersonalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов");
+} else if(PersonalMovieDB.count >= 10 & PersonalMovieDB.count < 30){
+    alert("Вы классический зритель");
+} else if(PersonalMovieDB.count >= 30){
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+console.log(PersonalMovieDB);
+
